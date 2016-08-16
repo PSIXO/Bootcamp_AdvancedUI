3b86da62ed87542e8185d802857eaf30760c4e43
PSIXO <appsolutistta@gmail.com>
2016-08-16 17:47:10 +0200

    ShareProvider

--- app/src/main/java/advancedui/android/bootcamp/end/advancedui/MainActivity.java
+++ app/src/main/java/advancedui/android/bootcamp/end/advancedui/MainActivity.java
@@ -1,14 +1,21 @@
 package advancedui.android.bootcamp.end.advancedui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbarTop;
+    private TextView text;
     private Toolbar toolbarBottom;
 
     @Override
@@ -18,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         toolbarTop = (Toolbar) findViewById(R.id.toolbar_top);
         setSupportActionBar(toolbarTop);
+
+        text = (TextView) findViewById(R.id.text_content);
 
         toolbarBottom = (Toolbar) findViewById(R.id.toolbar_bottom);
         toolbarBottom.inflateMenu(R.menu.bottom);
@@ -31,4 +40,18 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void onActionAlert(MenuItem item) {
+        Toast.makeText(MainActivity.this, "On Alert", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        Intent intentShare = new Intent();
+        intentShare.setAction(Intent.ACTION_SEND);
+        intentShare.setType("text/plain");
+        intentShare.putExtra(Intent.EXTRA_TEXT, text.getText().toString());
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menu.findItem(R.id.menu_item_share));
+        mShareActionProvider.setShareIntent(intentShare);
+        return true;
+    }
 }
