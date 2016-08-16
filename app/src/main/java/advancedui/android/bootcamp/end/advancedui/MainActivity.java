59c53728afc836d25e3d611036199fc3771b3bc4
PSIXO <appsolutistta@gmail.com>
2016-08-16 16:28:04 +0200

    Action bar

--- app/src/main/java/advancedui/android/bootcamp/end/advancedui/MainActivity.java
+++ app/src/main/java/advancedui/android/bootcamp/end/advancedui/MainActivity.java
@@ -1,13 +1,22 @@
 package advancedui.android.bootcamp.end.advancedui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main, menu);
+        return true;
     }
 }
