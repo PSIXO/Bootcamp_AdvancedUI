78fb8fe222be1bda7d2d4204f61ee870f9cfa125
PSIXO <appsolutistta@gmail.com>
2016-08-16 17:18:03 +0200

    Toolbar

--- app/src/main/java/advancedui/android/bootcamp/end/advancedui/MainActivity.java
+++ app/src/main/java/advancedui/android/bootcamp/end/advancedui/MainActivity.java
@@ -1,17 +1,28 @@
 package advancedui.android.bootcamp.end.advancedui;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbarTop;
+    private Toolbar toolbarBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbarTop = (Toolbar) findViewById(R.id.toolbar_top);
+        setSupportActionBar(toolbarTop);
+
+        toolbarBottom = (Toolbar) findViewById(R.id.toolbar_bottom);
+        toolbarBottom.inflateMenu(R.menu.bottom);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,4 +30,5 @@ public class MainActivity extends Activity {
         inflater.inflate(R.menu.activity_main, menu);
         return true;
     }
+
 }
